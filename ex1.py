14fc0e59f6b29a7dee3b8bcbec869e370e749ed3
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-14 13:46:21 -0300

    feat: update

--- ex1.py
+++ ex1.py
@@ -19,7 +19,7 @@
 
 # valorInicial = float(input("Qual seu valor inicial?"))
 # tempoAplicacao = float(input("Por quantos meses pretende aplicar?"))
-# taxaJuros = 20/100*100
+# taxaJuros = 14/100*100
 
 # juros = valorInicial*taxaJuros*tempoAplicacao
 # valorFinal = valorInicial + juros
@@ -32,10 +32,10 @@
 # o Calcule a média simples (soma das notas dividida por 3).
 # o Exiba a média.
 
-# nota1 = float(input("Digite a nota 1:"))
-# nota2 = float(input("Digite a nota 2:"))
-# nota3 = float(input("Digite a nota 3:"))
-# media = (nota1+nota2+nota3)/3
+nota1 = float(input("Digite a nota 1:"))
+nota2 = float(input("Digite a nota 2:"))
+nota3 = float(input("Digite a nota 3:"))
+media = (nota1+nota2+nota3)/3
 
-# print("A média do aluno é:", media)
+print("A média do aluno é:", media)
 
