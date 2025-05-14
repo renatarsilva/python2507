ce3167f6d270838b553f8e3da8e62ab98b5e4cf9
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-14 13:46:44 -0300

    feat: update

--- ex1.py
+++ ex1.py
@@ -32,10 +32,10 @@
 # o Calcule a média simples (soma das notas dividida por 3).
 # o Exiba a média.
 
-nota1 = float(input("Digite a nota 1:"))
-nota2 = float(input("Digite a nota 2:"))
-nota3 = float(input("Digite a nota 3:"))
-media = (nota1+nota2+nota3)/3
+# nota1 = float(input("Digite a nota 1:"))
+# nota2 = float(input("Digite a nota 2:"))
+# nota3 = float(input("Digite a nota 3:"))
+# media = (nota1+nota2+nota3)/3
 
-print("A média do aluno é:", media)
+# print("A média do aluno é:", media)
 
