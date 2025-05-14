631c176c440e5122b87b01d6d83b9df41fb70573
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-14 13:40:30 -0300

    Adiciona ex1.py

---
+++ ex1.py
@@ -0,0 +1,41 @@
+# Desconto:
+# o Defina o preço original de um produto e o percentual de desconto.
+# o Calcule o valor do desconto e o preço final.
+# o Exiba os resultados.
+
+# produto = "sapato social feminino"
+# precoOriginal = 100.00
+# percentualDesconto = 20/100*100
+# valorComDesconto = precoOriginal - percentualDesconto
+
+# print("Oferta master na loja da Maki!!",produto, "por R$", precoOriginal, "pagando no Pix, você ganha", percentualDesconto, "de desconto, e só paga R$", valorComDesconto)
+
+
+# 5. Juros Simples:
+# o Defina o valor inicial, a taxa de juros e o tempo de aplicação.
+# o Calcule os juros simples usando a fórmula: juros = valor_inicial * taxa *
+# tempo.
+# o Determine o valor final da aplicação.
+
+# valorInicial = float(input("Qual seu valor inicial?"))
+# tempoAplicacao = float(input("Por quantos meses pretende aplicar?"))
+# taxaJuros = 20/100*100
+
+# juros = valorInicial*taxaJuros*tempoAplicacao
+# valorFinal = valorInicial + juros
+
+# print("Supondo que você irá aplicar R$", valorInicial, "por", tempoAplicacao, "meses, com uma taxa de", taxaJuros, "%, ao mês. 100O valor do seu investimento final será de R$",valorFinal)
+
+
+# 6. Média Aritmética:
+# o Defina três notas.
+# o Calcule a média simples (soma das notas dividida por 3).
+# o Exiba a média.
+
+# nota1 = float(input("Digite a nota 1:"))
+# nota2 = float(input("Digite a nota 2:"))
+# nota3 = float(input("Digite a nota 3:"))
+# media = (nota1+nota2+nota3)/3
+
+# print("A média do aluno é:", media)
+
