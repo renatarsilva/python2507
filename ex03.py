625089c9c03f872da279fc38f06d1a7ca7f5c013
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-18 10:57:38 -0300

    feat:update

---
+++ ex03.py
@@ -0,0 +1,5 @@
+# DESAFIO 02
+"""
+Escreva um programa que leia dois caracteres e imprima-os na tela da seguinte forma:
+O usuário digitou (caractere1) e (caractere2)!.
+"""
---
+++ ex2.py
@@ -0,0 +1,9 @@
+# DESAFIO 01
+# """
+# Crie um programa para efetuar a leitura de um número inteiro e
+# apresentar o resultado do quadrado desse número
+# """
+
+numero = int(input("Digite um número inteiro:"))
+numero *= numero
+print(numero)
