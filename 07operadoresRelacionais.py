3261e82d09c275989a55612545a0c9a41bb60dc7
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-18 10:53:04 -0300

    feat:update operadores relacionais

---
+++ 07operadoresRelacionais.py
@@ -0,0 +1,10 @@
+# Operadores relacionais ou de comparação
+a = 10
+b = 3
+
+print(a == b)  # False  Igual
+print(a != b)  # True   Diferente
+print(a >  b)  # True   Maior
+print(a <  b)  # False  Menor
+print(a >= b)  # True   Maior ou igual
+print(a <= b)  # False  Menor ou igual
