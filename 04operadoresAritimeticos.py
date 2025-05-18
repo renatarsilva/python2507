95c255071c6d96cd4782f27a9672ea4d5c8c2fbe
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-18 10:52:07 -0300

    feat: update operadores aritimeticos

---
+++ 04operadoresAritimeticos.py
@@ -0,0 +1,13 @@
+# Operadores aritméticos
+
+a = 10
+b = 3
+
+print(a +  b)     # 13      (adição)
+print(a -  b)     # 7       (subtração)
+print(a *  b)     # 30      (multiplicação)
+print(a /  b)     # 3.3333  (divisão)
+print(a // b)     # 3       (divisão inteira)
+print(a %  b)     # 1       (módulo/resto da divisão)
+print(a ** b)     # 1000    (exponenciação)
+print(a ** (1/b)) # 2.14    (radiciação)
