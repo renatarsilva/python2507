f7f329e4cd9918417be16e015a96fc5d8b36ebbc
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-18 10:49:24 -0300

    feat: update introducao

---
+++ 01introducao.py
@@ -0,0 +1,51 @@
+print('HELLO WORLD')
+print("my name is Renata, but my nickname is Maki")
+
+"""Algoritmo lanche
+Passo 01: abrir armario
+PAsso 02: Pegar prato
+Passo 03: Colocar prato sob a mesa
+Passo 04: Fechar armario
+Passo 05: Abrir saco de pão
+Passo 06: Pegar uma fatia de pão
+Passo 07: Coloca-la sobre o prato
+Passo 08: Abrir geladeira
+Passo 09: pegar alface, presunto, queijo e tomate
+Passo 10: Fechar geladeira
+Passo 11: Pegar uma folha de alface
+Passo 12: Colocar folha de alface sobre o pao
+Passo 13: Colocar fatia de presunto sobre folha de alface
+Passo 14: Colocar fatia de queijo sobre fatia de presunto
+Passo 15: Colocar pedacos de tomate sob fatia de queijo
+Passo 16: Pegar fatia de pao
+Passo 17: Colocar sob fatia de queijo
+PAsso 18: servir lanche
+
+Abstração lógica - cafeteira
+
+Botao on/off - Liga e desliga
+motor
+Reservatorio de agua - armazena água
+condensador - condesa vapor
+deposito - para pó de café
+Filtro - Filtrar o pó
+jarra - para armazenar o liquido, pós filtro
+
+Lógica - barril
+
+Passo 01: Despejar todo líquido do barril C, no barril A
+Passo 02: Despejar 1 Litro do líquido do barril B, no barril A
+Passo 03: Verificar se barris A e B. estão com 4 litros de líquido em cada
+Passo 04: Verificar se barril C, está vazio
+"""
+
+
+
+
+
+
+
+
+
+
+
