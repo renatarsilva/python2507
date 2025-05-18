7c05db05941682d06109f988ac990db90b642278
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-18 10:51:45 -0300

    feat: update condicionais

---
+++ 03condicionais.py
@@ -0,0 +1,50 @@
+#CONDICIONAIS
+
+nota = float(input("Digite a nota1:"))
+frequencia = int(input("Digite frequencia:"))
+
+if nota > 6 and frequencia >= 75:
+    print('aprovado')
+else:
+    print('Reprovado')
+
+# DIAGRAMA MÉDIA
+
+nota1 = 0
+nota2 = 0
+media = 0
+
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+
+media = (nota1 + nota2)/2
+
+if media >= 5:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+# DIAGRAMA MÉDIA
+
+nota1 = 0
+nota2 = 0
+media = 0
+
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+
+media = (nota1 + nota2)/2
+
+print("Aprovado" if media >= 5 else "Reprovado")
+
+# EXEMPLO 01
+
+
+nota = 49
+ausencia = 15
+
+
+if nota >= 50 and ausencia <= 15:
+    print("Aprovado")
+else:
+    print("Reprovado")
