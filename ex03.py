f51fda3d56b83ae7c9fdc44b494457f295cad57c
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-18 12:24:46 -0300

    feat:update desafio

--- ex03.py
+++ ex03.py
@@ -3,3 +3,7 @@
 Escreva um programa que leia dois caracteres e imprima-os na tela da seguinte forma:
 O usuário digitou (caractere1) e (caractere2)!.
 """
+
+caractere1 = input("Digite um caractere:")
+caractere2 = input("Digite outro caractere:")
+print(f'O usuário digitou a {caractere1} e {caractere2}')
