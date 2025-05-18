4d963d4ce049255f224f6d8dcef7309d7ec3ab02
Renata <renata.rodriguesdasilva@outlook.com>
2025-05-18 10:52:25 -0300

    feat: update operadores de atribuicao

---
+++ 05operadoresDeAtribuicao.py
@@ -0,0 +1,29 @@
+# Operadores de atribuição
+
+a = 10
+a += 2  # a = a + 2
+print(a) # 12
+
+b = 10
+b -= 2  # b = b - 2
+print(b) # 8
+
+c = 10
+c *= c  # c = c * c
+print(c) # 100
+
+d = 10
+d /= 2 # d = d / 2
+print(d) # 5.0
+
+d = 10
+d **= 2 # d = d ** 2
+print(d) # 100
+
+d = 10
+d //= 2
+print(d)
+
+d = 10
+d %= 2
+print(d) # 5.0
